68302ac256a3d8002d36bc71e82ad1d67075312f
Faahm <fatimahussien2@gmail.com>
2022-11-16 20:29:54 +0800

    Finish RPS

--- index.js
+++ index.js
@@ -17,26 +17,46 @@ function getComputerChoice() {
 let computerScore = 0;
 let playerScore = 0;
 const resultP = document.createElement("p");
+const computerScoreDOM = document.querySelector('#computerScore');
+const playerScoreDOM = document.querySelector('#playerScore');
+counter = 0;
 
 function playRound(e) {
-  let matchResult = "";
-  const computerSelection = getComputerChoice();
-  const playerSelection = e.target.id;
-  if (playerSelection == computerSelection) {
-    matchResult = "It's a tie!";
-  } else if (playerSelection == "cryo" && computerSelection == "pyro") {
-    matchResult = `You lose! ${computerSelection} beats ${playerSelection}`;
-  } else if (playerSelection == "pyro" && computerSelection == "hydro") {
-    matchResult = `You lose! ${computerSelection} beats ${playerSelection}`;
-  } else if (playerSelection == "hydro" && computerSelection == "cryo") {
-    matchResult = `You lose! ${computerSelection} beats ${playerSelection}`;
-  } else {
-    matchResult = `You win! ${playerSelection} beats ${computerSelection}`;
+    let matchResult = "";
+    const computerSelection = getComputerChoice();
+    const playerSelection = e.target.id;
+    
+  if(counter < 5) {
+    if (playerSelection == computerSelection) {
+      matchResult = "It's a tie!";
+    } else if (playerSelection == "cryo" && computerSelection == "pyro") {
+      matchResult = `The Abyss Order used ${computerSelection} on the Traveler's ${playerSelection}. The Traveler lost.`;
+      computerScore += 1;
+    } else if (playerSelection == "pyro" && computerSelection == "hydro") {
+      matchResult = `The Abyss Order used ${computerSelection} on the Traveler's ${playerSelection}. The Traveler lost.`;
+      computerScore += 1;
+    } else if (playerSelection == "hydro" && computerSelection == "cryo") {
+      matchResult = `The Abyss Order used ${computerSelection} on the Traveler's ${playerSelection}. The Traveler lost.`;
+      computerScore += 1;
+    } else {
+      matchResult = `The Traveler used ${playerSelection} on the Abyss Order's ${computerSelection}. The Abyss lost.`;
+      playerScore += 1;
+    }
+    computerScoreDOM.textContent = computerScore;
+    playerScoreDOM.textContent  = playerScore;
+    const domContainer = document.querySelector(".dom-container");
+    resultP.textContent = matchResult;
+    domContainer.appendChild(resultP);
+    counter += 1;
   }
-
-  const domContainer = document.querySelector("#dom-container");
-  resultP.textContent = matchResult;
-  domContainer.appendChild(resultP);
+  else {
+    if(playerScore < computerScore)
+      resultP.textContent = 'Final winner is the Abyss Order!';
+    else if(playerScore > computerScore)
+      resultP.textContent = 'Final winner is the Traveler!';
+    else
+    resultP.textContent = "Ooh, it's a tie between the Abyss Order and the Traveler!";
+  }
 }
 
 const elements = Array.from(document.querySelectorAll(".element"));
